e4a285fb111fe6928e3bf8762a284568dcf97322
Ryoichi Ben <ryoichi.ben@RyoichinoMacBook-Air.local>
2025-12-30 01:18:37 +0900

    add project page

---
+++ app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function ProjectsPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1 style={{ fontSize: 24, fontWeight: 700 }}>Projects</h1>
+      <p>This is /projects page</p>
+    </main>
+  );
+}
